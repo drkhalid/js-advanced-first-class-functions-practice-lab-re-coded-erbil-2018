630628bfc48278883de3eca3f10c8b8f416b40b6
khalid jamal <kh.jamal95@gmail.com>
2018-07-26 10:19:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+
+const logDriverNames = function (drivers){
+  drivers.forEach(function(drivers){
+    console.log(drivers.name)
+  })
+}
+
+const logDriversByHometown = function (drivers, location){
+    drivers.forEach(function(driver){ 
+      if (driver.hometown === location){console.log(driver.name)}
+      
+    })
+  }
+
+const driversByRevenue = function(){
+  
+}
+
+
+
+
+const totalRevenue = function (drivers){
+  return drivers.reduce(function(total, current){return current.revenue + total;},0)
+}
+
+const averageRevenue = function (drivers){return totalRevenue(drivers)/drivers.length}
+
+
+
+
